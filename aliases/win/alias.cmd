ef1365afa6a78a7678bfeefe70491e0b67c89a9c
Jesse Koldewijn <jesse@iungo.nl>
2023-04-11 17:06:50 +0200

    cleanup: removed some unused lines from alias.cmd file

--- aliases/win/alias.cmd
+++ aliases/win/alias.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-:: Making sure path is set
 set PATH=%PATH%;
 
 :: Commands
@@ -15,10 +14,8 @@ set PATH=%PATH%;
 :: Inject clink runtime into shell
     call clink inject | find /i "Clink already loaded in process"
     if not errorlevel 1 (
-        :: clink is already running
         cls
     ) else (
-        :: clink isn't running yet
         call clink inject
         call cls
     )
